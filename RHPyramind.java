7aee16704aaaf9e514f20ff1ef423cc7927f297d
UDIT KUMAR NAMA <namaudit12@gmail.com>
2025-06-10 00:39:51 +0530

    Challenge 1

---
+++ RHPyramind.java
@@ -0,0 +1,9 @@
+public class RHPyramind {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
