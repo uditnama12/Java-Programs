845b657b4d4116d19a4650a8af0d343538da996f
UDIT KUMAR NAMA <namaudit12@gmail.com>
2025-06-11 00:50:20 +0530

    User Input Programs

---
+++ UserInput.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter your name: ");
+        String name = input.nextLine();
+        System.out.println(" Good morning " + name);
+        System.out.print(name + " Also tell me your age: ");
+        int age = input.nextInt();
+        System.out.println(" Your age is:" + age);
+
+        System.out.println("Please Enter you salary: ");
+        int a = input.nextInt();
+       // System.out.println(" Please enter your salary: " + a);
+       
+       System.out.println(" Please enter your PF Amunt: ");
+        int b = input.nextInt();
+        
+         int c = a-b;
+        System.out.println("Your actual salary is : " + c);
+
+        System.out.println(" Please enter your ESIC amount : ");
+        int E = input.nextInt();
+
+        int Total = c + E;
+        System.out.println("Your total Inhand Salary is : " + Total);
+        
+
+    }
+}
--- escape.java
+++ escape.java
@@ -1,8 +1,8 @@
 public class escape {
     public static void main(String[] args) {
-      //  System.out.println("*\n**\n***\n****\n*****");
+      System.out.println("*\n**\n***\n****\n*****");
 
-     // System.out.println("*****\n****\n***\n**\n*");
+      System.out.println("*****\n****\n***\n**\n*");
 
      System.out.println("    *\n   **\n  ***\n ****\n*****");
     }
