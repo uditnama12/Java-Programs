1d069e14c1709f571214e31aa147c46c58e3d42a
UDIT KUMAR NAMA <namaudit12@gmail.com>
2025-06-10 23:44:03 +0530

    Division of 2 integers

---
+++ Division.java
@@ -0,0 +1,10 @@
+public class Division{
+    public static void main(String[] args) {
+        int a= 79;
+        int b=25;
+        int c= a/b;
+        System.out.println("The number will be:" );
+        System.out.println(c);
+        
+    }
+}
