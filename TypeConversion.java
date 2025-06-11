0440458e241d93aea9b5421c4e9ff8d4b63a215d
UDIT KUMAR NAMA <namaudit12@gmail.com>
2025-06-11 23:56:30 +0530

    Type Conversion Program

---
+++ TypeConversion.java
@@ -0,0 +1,15 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        float a= 98;
+        System.out.println(a);
+
+
+        int b = (int) 7983.2f;
+        System.out.println(b);
+
+
+        double d = 123.345;
+        System.out.println(d);
+    }
+    
+}
